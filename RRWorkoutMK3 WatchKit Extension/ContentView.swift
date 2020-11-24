ad56c0b8ee97445a58bd4f8c7a337d68d9208199
Dan O'Leary <olearydaniel@me.com>
2020-11-24 09:41:14 -0600

    Initial Commit

---
+++ RRWorkoutMK3 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RRWorkoutMK3 WatchKit Extension
+//
+//  Created by Daniel O'Leary on 11/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RRWorkoutMK3 WatchKit Extension/RRWorkoutMK3App.swift
@@ -0,0 +1,19 @@
+//
+//  RRWorkoutMK3App.swift
+//  RRWorkoutMK3 WatchKit Extension
+//
+//  Created by Daniel O'Leary on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct RRWorkoutMK3App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
